c0a272453af5eed0b543ed48546e52e18faed251
Anastasia Zaika <z.anastasia11@gmail.com>
2022-11-24 16:41:23 -0800

    set axios call to retret data from api, populated dinamically inventory

--- src/components/warehouseInventoryList/WarehouseInventoryList.js
+++ src/components/warehouseInventoryList/WarehouseInventoryList.js
@@ -5,86 +5,108 @@ import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
 import sortIcon from "../../assets/icons/sort-24px.svg";
 import "./WarehouseInventoryList.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function WarehouseInventoryList() {
+export default function WarehouseInventoryList({ warehouseId }) {
+  const [warehouseInventoryList, setWarehouseInventoryList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/warehouses/${warehouseId}/inventories`)
+      .then((response) => {
+        setWarehouseInventoryList(response.data);
+      });
+  }, [warehouseId]);
+
   return (
     <section className="inventory">
-      <div className="inventory__list">
-        <div className="inventory__card">
-          {/* INVENTORY ITEM */}
-          <div className="inventory__item-wrapper">
-            <div className="inventory__item">
-              <div className="inventory__item-header">
-                <h5 className="inventory__item-text ">INVENTORY ITEM</h5>
-                <img
-                  className="inventory__sort-sortIcon"
-                  src={sortIcon}
-                  alt="sort icon"
-                />
-              </div>
-              <div className="inventory__item-thing">
-                <a className="inventory__item-link" href="">
-                  <p className="inventory__item-thing-text">Television</p>
+      {warehouseInventoryList?.map((warehouseInventory) => (
+        <div className="inventory__list" key ={warehouseInventory.item_name+warehouseInventory.id}>
+          <div className="inventory__card">
+            {/* INVENTORY ITEM */}
+            <div className="inventory__item-wrapper">
+              <div className="inventory__item">
+                <div className="inventory__item-header">
+                  <h5 className="inventory__item-text ">INVENTORY ITEM</h5>
+                  <img
+                    className="inventory__sort-sortIcon"
+                    src={sortIcon}
+                    alt="sort icon"
+                  />
+                </div>
+                <div className="inventory__item-thing">
+                  <a className="inventory__item-link" href="">
+                    <p className="inventory__item-thing-text">
+                      {warehouseInventory.item_name}
+                    </p>
 
-                  <img src={chevronIcon} alt="more than sign" />
-                </a>
+                    <img src={chevronIcon} alt="more than sign" />
+                  </a>
+                </div>
+              </div>
+
+              {/* INVENTORY STATUS */}
+              <div className="inventory__status">
+                <div className="inventory__status-header">
+                  <h5 className="inventory__status-text">STATUS</h5>
+                  <img
+                    className="inventory__sort-sortIcon"
+                    src={sortIcon}
+                    alt="sort icon"
+                  />
+                </div>
+                <p className="inventory__status-stock">
+                  {warehouseInventory.status}
+                </p>
               </div>
             </div>
 
-            {/* INVENTORY STATUS */}
-            <div className="inventory__status">
-              <div className="inventory__status-header">
-                <h5 className="inventory__status-text">STATUS</h5>
-                <img
-                  className="inventory__sort-sortIcon"
-                  src={sortIcon}
-                  alt="sort icon"
-                />
+            <div className="inventory__category-wrapper">
+              {/* INVENTORY CATEGORY */}
+              <div className="inventory__category">
+                <div className="inventory__category-header">
+                  <h5 className="inventory__category-text">CATEGORY</h5>
+                </div>
+                <p className="inventory__category-name">
+                  {warehouseInventory.category}
+                </p>
               </div>
-              <p className="inventory__status-stock">IN STOCK</p>
-            </div>
-          </div>
 
-          <div className="inventory__category-wrapper">
-            {/* INVENTORY CATEGORY */}
-            <div className="inventory__category">
-              <div className="inventory__category-header">
-                <h5 className="inventory__category-text">CATEGORY</h5>
+              {/* INVENTORY QTY */}
+              <div className="inventory__qty">
+                <div className="inventory__qty-header">
+                  <h5 className="inventory__qty-text">QTY</h5>
+                  <img
+                    className="inventory__sort-sortIcon"
+                    src={sortIcon}
+                    alt="sort icon"
+                  />
+                </div>
+                <p className="inventory__qty-num">
+                  {warehouseInventory.quantity}
+                </p>
               </div>
-              <p className="inventory__category-name">Electronics</p>
             </div>
 
-            {/* INVENTORY QTY */}
-            <div className="inventory__qty">
-              <div className="inventory__qty-header">
-                <h5 className="inventory__qty-text">QTY</h5>
-                <img
-                  className="inventory__sort-sortIcon"
-                  src={sortIcon}
-                  alt="sort icon"
-                />
+            {/* INVENTORY ACTIONS BUTTONS */}
+            <div className="inventory__actions">
+              <div className="inventory__actions-header">
+                <h5 className="inventory__actions-text">ACTIONS</h5>
               </div>
-              <p className="inventory__qty-num">500</p>
-            </div>
-          </div>
 
-          {/* INVENTORY ACTIONS BUTTONS */}
-          <div className="inventory__actions">
-            <div className="inventory__actions-header">
-              <h5 className="inventory__actions-text">ACTIONS</h5>
-            </div>
-
-            <div className="inventory__actions-btns">
-              <button>
-                <img src={deleteIcon} alt="delete" />
-              </button>
-              <button>
-                <img src={editIcon} alt="edit" />
-              </button>
+              <div className="inventory__actions-btns">
+                <button>
+                  <img src={deleteIcon} alt="delete" />
+                </button>
+                <button>
+                  <img src={editIcon} alt="edit" />
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
     </section>
   );
 }
--- src/components/warehouseSingle/WarehouseSingle.js
+++ src/components/warehouseSingle/WarehouseSingle.js
@@ -2,15 +2,19 @@
 
 import WarehouseDetails from "../warehouseDetails/WarehouseDetails";
 import WarehouseInventoryList from "../warehouseInventoryList/WarehouseInventoryList";
-
+import { useParams } from "react-router-dom";
 
 
 export default function WarehouseSingle() {
+
+  const { warehouseId } = useParams();
+
+
   return (
     <>
       <h1>Single Warehouse Section Header</h1>
       <WarehouseDetails />
-      <WarehouseInventoryList />
+      <WarehouseInventoryList warehouseId = {warehouseId}/>
     </>
   );
 }
