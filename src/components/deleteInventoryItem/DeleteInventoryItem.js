786c06eca45cbac31886670471a1044a8c565d97
Vivi Schulz <haweirh@gmail.com>
2022-11-25 14:23:38 -0800

    Completed functionality and styling

--- src/components/deleteInventoryItem/DeleteInventoryItem.js
+++ src/components/deleteInventoryItem/DeleteInventoryItem.js
@@ -3,6 +3,7 @@ import "./DeleteInventoryItem.scss";
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
 import axios from "axios";
+import closeIcon from "../../assets/icons/close-24px.svg";
 
 const DisplayDeletePopup = (props) => {
   const formRef = useRef();
@@ -43,18 +44,36 @@ const DisplayDeletePopup = (props) => {
   return props.trigger ? (
     <form className="deleteInventory__popup" ref={formRef}>
       <div className="deleteInventory__pop-inner">
-        <p>Delete {props.showDeleteInventoryItem} inventory item?</p>
-        <p>
-          Please confirm that you'd like to delete {props.showDeleteInventoryItem} from the inventory list. You won't be able to undo this action.
-        </p>
-        <button
-          className="deleteInventory__close-btn"
-          onClick={() => props.setTrigger(false)}
-        >
-          close
-        </button>
-        <button onClick={handleCancel}>Cancel</button>
-        <button onClick={handleDelete}>Delete</button>
+        <div className="deleteInventory__main-group">
+          <h1 className="deleteInventory__title">
+            Delete {props.showDeleteInventoryItem} inventory item?
+          </h1>
+          <p className="deleteInventory__p">
+            Please confirm that you'd like to delete{" "}
+            {props.showDeleteInventoryItem} from the inventory list. You won't
+            be able to undo this action.
+          </p>
+          <button
+            className=" deleteInventory__close-btn"
+            onClick={() => props.setTrigger(false)}
+          >
+            <img src={closeIcon} alt="close icon: close popup window" />
+          </button>
+        </div>
+        <div className="deleteInventory__btn-group ">
+          <button
+            onClick={handleCancel}
+            className="deleteInventory__btn deleteInventory__cancel-btn"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={handleDelete}
+            className="deleteInventory__btn  deleteInventory__delete-btn"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </form>
   ) : (
