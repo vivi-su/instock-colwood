959fc907990468a8093ae386e2b6672cdf204cb5
Vivi Schulz <haweirh@gmail.com>
2022-11-25 21:43:51 -0800

    Updated the deleteinventory item

--- src/components/deleteInventoryItem/DeleteInventoryItem.js
+++ src/components/deleteInventoryItem/DeleteInventoryItem.js
@@ -1,21 +1,22 @@
-import Inventory from "../../pages/inventory/Inventory";
 import "./DeleteInventoryItem.scss";
-import { useParams, useNavigate } from "react-router-dom";
-import { useEffect, useState, useRef } from "react";
+import { useParams, useNavigate, Link, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import closeIcon from "../../assets/icons/close-24px.svg";
 
-const DisplayDeletePopup = (props) => {
-  const formRef = useRef();
+export default function DeleteInventoryItem({
+  handleDeleteItem,
+  inventoryItemsList,
+}) {
   const navigate = useNavigate();
   const { itemId } = useParams();
-  const [deletePopup, setDeletePopup] = useState(false);
-  const [deleteInventoryItem, SetDeleteInventoryItem] = useState([]);
+  const [showDeleteInventoryItem, setShowDeleteInventoryItem] = useState();
 
-  const handleCancel = (event) => {
-    event.preventDefault();
-    navigate("/inventory");
-  };
+  useEffect(() => {
+    setShowDeleteInventoryItem(
+      inventoryItemsList?.find((inventoryItem) => inventoryItem.id === itemId)
+    );
+  }, [itemId, setShowDeleteInventoryItem, inventoryItemsList]);
 
   const handleDelete = (event) => {
     event.preventDefault();
@@ -24,14 +25,10 @@ const DisplayDeletePopup = (props) => {
         return;
       }
       try {
-        const { data } = await axios.delete(
-          `http://localhost:8080/inventories/${itemId}`
-        );
-        SetDeleteInventoryItem(data);
-        alert(
-          `Your ${props.showDeleteInventoryItem} item is successfully deleted!`
-        );
-        window.location.reload(false);
+        await axios.delete(`http://localhost:8080/inventories/${itemId}`);
+        alert(`Your ${showDeleteInventoryItem} item is successfully deleted!`);
+        handleDeleteItem(itemId);
+        navigate("/inventory");
       } catch (err) {
         console.log(`${itemId} does not exist`, err);
       }
@@ -39,81 +36,45 @@ const DisplayDeletePopup = (props) => {
     deleteInventoryItem();
   };
 
-  return props.trigger ? (
-    <form className="deleteInventory__popup" ref={formRef}>
-      <div className="deleteInventory__pop-inner">
-        <div className="deleteInventory__main-group">
-          <h1 className="deleteInventory__title">
-            Delete {props.showDeleteInventoryItem} inventory item?
-          </h1>
-          <p className="deleteInventory__p">
-            Please confirm that you'd like to delete{" "}
-            {props.showDeleteInventoryItem} from the inventory list. You won't
-            be able to undo this action.
-          </p>
-          <button
-            className=" deleteInventory__close-btn"
-            onClick={() => props.setTrigger(false)}
-          >
-            <img src={closeIcon} alt="close icon: close popup window" />
-          </button>
-        </div>
-        <div className="deleteInventory__btn-group ">
-          <button
-            onClick={handleCancel}
-            className="deleteInventory__btn deleteInventory__cancel-btn"
-          >
-            Cancel
-          </button>
-          <button
-            onClick={handleDelete}
-            className="deleteInventory__btn  deleteInventory__delete-btn"
-          >
-            Delete
-          </button>
-        </div>
-      </div>
-    </form>
-  ) : (
-    ""
-  );
-};
-
-export default function DeleteInventoryItem() {
-     const { itemId } = useParams();
-     const [deletePopup, setDeletePopup] = useState(false);
-     const [showDeleteInventoryItem, SetShowDeleteInventoryItem] = useState([]);
-     
-     
-     useEffect(() => {
-       const fetchInventoryItem = async () => {
-         if (!itemId) {
-           return;
-         }
-         try {
-           const { data } = await axios.get(
-             `http://localhost:8080/inventories/${itemId}`
-           );
-           setDeletePopup(true);
-           SetShowDeleteInventoryItem(data.item_name);
-         } catch (err) {
-           console.log(`${itemId} does not exist`, err);
-         }
-       };
-
-       fetchInventoryItem();
-     }, [itemId, SetShowDeleteInventoryItem, showDeleteInventoryItem]);
-    
-
   return (
     <>
-      <DisplayDeletePopup
-        trigger={deletePopup}
-        setTrigger={setDeletePopup}
-        showDeleteInventoryItem={showDeleteInventoryItem}
-      ></DisplayDeletePopup>
-
-      <Inventory />
+      <Outlet />
+      <div className="deleteInventory__popup">
+        <div className="deleteInventory__pop-inner">
+          <div className="deleteInventory__main-group">
+            {showDeleteInventoryItem?.item_name && (
+              <h1 className="deleteInventory__title">
+                Delete {showDeleteInventoryItem?.item_name} inventory item?
+              </h1>
+            )}
+            {showDeleteInventoryItem?.item_name && (
+              <p className="deleteInventory__p">
+                Please confirm that you'd like to delete{" "}
+                {showDeleteInventoryItem?.item_name} from the inventory list.
+                You won't be able to undo this action.
+              </p>
+            )}
+            <Link to={`/inventory`}>
+              <div className=" deleteInventory__close-btn">
+                <img src={closeIcon} alt="close icon: close popup window" />
+              </div>
+            </Link>
+          </div>
+          <div className="deleteInventory__btn-group ">
+            <Link to={`/inventory`}>
+              <button className="deleteInventory__btn deleteInventory__cancel-btn">
+                Cancel
+              </button>
+            </Link>
+            <button
+              onClick={handleDelete}
+              className="deleteInventory__btn  deleteInventory__delete-btn"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
