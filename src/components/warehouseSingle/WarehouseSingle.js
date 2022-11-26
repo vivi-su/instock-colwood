f72e8a812729b9a7f6a54c65b3e04c8e37915208
Priscila M. Moura <priscila.moura@gmail.com>
2022-11-26 03:32:31 -0800

    styled single warehouse page container for 3 screen sizes

--- src/components/warehouseSingle/WarehouseSingle.js
+++ src/components/warehouseSingle/WarehouseSingle.js
@@ -1,5 +1,5 @@
 //Parent component to ticket 10 and ticket 11
-
+import "./WarehouseSingle.scss";
 import WarehouseDetails from "../warehouseDetails/WarehouseDetails";
 import WarehouseInventoryList from "../warehouseInventoryList/WarehouseInventoryList";
 import { useParams } from "react-router-dom";
@@ -8,9 +8,9 @@ export default function WarehouseSingle() {
   const { warehouseId } = useParams();
 
   return (
-    <>
+    <section className="warehouseSingle-container">
       <WarehouseDetails warehouseId={warehouseId} />
       <WarehouseInventoryList warehouseId={warehouseId} />
-    </>
+    </section>
   );
 }
