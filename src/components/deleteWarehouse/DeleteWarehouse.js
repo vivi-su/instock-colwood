4faa15c73cbbb53c9cc5e0795aea5c4c4c7b7341
Anastasia Zaika <z.anastasia11@gmail.com>
2022-11-26 18:10:00 -0800

    create fn for delete warehouse page

--- src/components/deleteWarehouse/DeleteWarehouse.js
+++ src/components/deleteWarehouse/DeleteWarehouse.js
@@ -1,125 +1,95 @@
 //Ticket 13
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet, useParams, useNavigate } from "react-router-dom";
 import Exit from "../../assets/icons/close-24px.svg";
 import "./DeleteWarehouse.scss";
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const DisplayDeletePopup = (props) => {
-  //const formRef = useRef();
+export default function DeleteWarehouse({
+ handleDeleteWarehouse,
+  warehouseList,
+}) {
   const navigate = useNavigate();
   const { warehouseId } = useParams();
-  const [deletePopup, setDeletePopup] = useState(false);
-  const [deleteWarehouse, SetDeleteWarehouse] = useState([]);
-  //console.log("showDeleteWarehouse",props.showDeleteWarehouse);
+  const [showDeleteWarehouse, setShowDeleteWarehouse] = useState({});
 
-  const handleCancel = (event) => {
-    event.preventDefault();
-    navigate("/warehouses");
-  };
+   useEffect(() => {
+    setShowDeleteWarehouse(
+      warehouseList?.find((warehouse) => warehouse.id === warehouseId)
+    );
+  }, [
+    warehouseId,
+    setShowDeleteWarehouse,
+    warehouseList
+  ]);
 
-  const handleDelete = (event) => {
+const handleDelete = (event) => {
     event.preventDefault();
+
     const deleteWarehouse = async () => {
       if (!warehouseId) {
         return;
       }
       try {
-        const { data } = await axios.delete(
-          `http://localhost:8080/warehouses/${warehouseId}`
-        );
-        SetDeleteWarehouse(data);
-        setDeletePopup(false);
-        alert(
-          `${props.showDeleteInventoryItem} warehouse is successfully deleted!`
-        );
-        window.location.reload(false);
+        await axios.delete(`http://localhost:8080/warehouses/${warehouseId}`);
+       
+        handleDeleteWarehouse(warehouseId);
+        navigate("/warehouses");
       } catch (err) {
-        //console.log(`${warehouseId} does not exist`, err);
+        console.log(`${warehouseId} does not exist`, err);
       }
     };
     deleteWarehouse();
   };
 
-  return props.trigger ? (
-    <div className="opacity">
-      <div className="delete-warehouse">
-        <div className="delete-warehouse__container">
-          <Link to="/warehouses">
+
+
+return (
+  <>
+    <Outlet />
+    <div className="delete-warehouse__box">
+      <div className="delete-warehouse__popup">
+        <Link to={`/warehouses`}>
+          <button className="delete-warehouse__exit">
             <img
               src={Exit}
               alt="Exit Icon"
               className="delete-warehouse__icon"
             />
-          </Link>
-          <h1 className="delete-warehouse__heading">
-            Delete {props.showDeleteWarehouse} warehouse?
+          </button>
+        </Link>
+        <div className="delete-warehouse__heading">
+          <h1 className="delete-warehouse__heading-text">
+            Delete {showDeleteWarehouse?.warehouse_name} warehouse?
           </h1>
-          <p className="delete-warehouse__text">
+        </div>
+        <div className="delete-warehouse__details">
+          <p className="delete-warehouse__details-text">
             Please confirm that you’d like to delete the{" "}
-            {props.showDeleteWarehouse} from the list of warehouses. You won’t
-            be able to undo this action.
+            {showDeleteWarehouse?.warehouse_name} from the list of warehouses.
+            You won’t be able to undo this action.
           </p>
-          <div className="delete-warehouse__button-container">
-            <Link to="/warehouses" className="delete-warehouse__cancel">
-              <button
-                onClick={handleCancel}
-                className="delete-warehouse__button delete-warehouse__button-cancel"
-              >
-                Cancel
-              </button>
-            </Link>
-            <button
-              className="delete-warehouse__button delete-warehouse__button-delete"
-              onClick={handleDelete}
-            >
-              Delete
+        </div>
+        <div className="delete-warehouse__button-container">
+          <Link to={`warehouses`} className="delete-warehouse__button-cancel">
+            <button className="delete-warehouse__button delete-warehouse__cancel">
+              Cancel
             </button>
-          </div>
+          </Link>
+          <button
+            className="delete-warehouse__button delete-warehouse__delete"
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
         </div>
       </div>
     </div>
-  ) : (
-    ""
-  );
-};
-
-export default function DeleteWarehouse() {
-  const { warehouseId } = useParams();
-  const [deletePopup, setDeletePopup] = useState(false);
-  const [showDeleteWarehouse, SetShowDeleteWarehouse] = useState([]);
-
-  useEffect(() => {
-    const fetchWarehouse = async () => {
-      if (!warehouseId) {
-        return;
-      }
-      try {
-        const { data } = await axios.get(
-          `http://localhost:8080/warehouses/${warehouseId}`
-        );
-        setDeletePopup(true);
-        console.log(data.warehouse_name);
-        SetShowDeleteWarehouse(data.warehouse_name);
-        console.log(showDeleteWarehouse);
-      } catch (err) {
-        console.log(`${warehouseId} does not exist`, err);
-      }
-    };
-
-    fetchWarehouse();
-  }, [warehouseId, SetShowDeleteWarehouse, showDeleteWarehouse]);
-
-  return (
-    <>
-      <DisplayDeletePopup
-        trigger={deletePopup}
-        setTrigger={setDeletePopup}
-        showDeleteWarehouse={showDeleteWarehouse}
-      ></DisplayDeletePopup>
-      
-    </>
-  );
+  </>
+);
 }
+
+
+
+
