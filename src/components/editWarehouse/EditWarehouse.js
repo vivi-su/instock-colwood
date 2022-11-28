f659a73bc2ade90b89fa76c27bb3383d0339bf82
Vivi Schulz <haweirh@gmail.com>
2022-11-27 22:39:27 -0800

    Finished most validation

--- src/components/editWarehouse/EditWarehouse.js
+++ src/components/editWarehouse/EditWarehouse.js
@@ -9,7 +9,6 @@ import { useParams, Link } from "react-router-dom";
 export default function EditWarehouse() {
   const [selectedWarehouse, setSelectedWarehouse] = useState({});
   const { warehouseId } = useParams();
-  const [warehouseNameValid, setWarehouseNameValid] = useState(true); 
   const [warehouseName, setWarehouseName] = useState();
   const [address, setAddress]= useState();
   const [city, setCity]= useState();
@@ -53,31 +52,114 @@ export default function EditWarehouse() {
     selectedWarehouse.contact_email
   ]);
 
+const handleWarehouseNameChange = (e)=>{
+  setWarehouseName(e.target.value);
+};
 
+const handleAddressChange = (e) =>{
+  setAddress(e.target.value);
+}
 
+const handleCityChange = (e) =>{
+  setCity(e.target.value);
+}
 
+const handleCountryChange = (e) =>{
+  setCountry(e.target.value);
+} 
 
+const handleContactNameChange = (e) =>{
+  setContactName(e.target.value);
+}
 
-  const handleChange = (e)=>{
-     console.log(e.target.value);
-         const isWarehouseNameValid = () => {
-           if (!warehouseName) {
-             return true;
-           }
-           return false;
-         };
-alert(isWarehouseNameValid(warehouseName));
+const handlePositionChange = (e) => {
+  setContactPosition(e.target.value);
+};
+
+const handleContactPhoneChange = (e) =>{
+  setContactPhone(e.target.value);
+}
+
+const handleContactEmailChange = (e) =>{
+  setContactEmail(e.target.value);
+}
+
+   
+const isWarehouseNameValid = () => {
+      if (warehouseName==="") {
+          return false;
+      }
+          return true;
+  };
+
+const isAddressValid = () => {
+      if (address === "") {
+        return false;
+      }
+      return true;
+};
+
+const isCityValid = () => {
+  if (city === "") {
+    return false;
   }
+  return true;
+};
+
+const isCountryValid = () =>{
+  if (country === ""){
+    return false;
+  }
+  return true;
+}
+
+const isContactNameValid = () => {
+  if (contactName === "") {
+    return false;
+  }
+  return true;
+};
+
+const isContactPositionValid = () => {
+  if (contactPosition === "") {
+    return false;
+  }
+  return true;
+};
+
+const isContactPhoneValid = () => {
+  
+  if (contactPhone ==="") {
+    return false;
+  }
+  return true;
+};
+
+const isContactEmailValid = ()=>{
+    if (contactEmail==="") {
+      return false;
+    }
+  return true;
+};
 
 
+
+
+  
   const handleEdit = (e) => {
     e.preventDefault();
 
-    // alert(warehouseNameValid);
- 
-    
-    if (warehouseNameValid) 
-      {
+
+    if (
+      isWarehouseNameValid(warehouseName) &&
+      isAddressValid(address) &&
+      isCityValid(city) &&
+      isCountryValid(country) &&
+      isContactNameValid(contactName) &&
+      isContactPositionValid(contactPosition) &&
+      isContactPhoneValid(contactPhone) &&
+      isContactEmailValid(contactEmail)
+    ) {
       const values = {
         warehouse_name: e.target.warehouseName.value,
         address: e.target.address.value,
@@ -94,10 +176,9 @@ alert(isWarehouseNameValid(warehouseName));
         alert(`The warehouse ${selectedWarehouse.warehouse_name} is updated!`);
         window.location.reload(false);
       });
-      }
-      else{
-        alert("required form");
-      }
+    } else {
+      alert("required form");
+    }
   };
   return (
     <>
@@ -119,14 +200,18 @@ alert(isWarehouseNameValid(warehouseName));
               <h2 className="edit-warehouse__details">Warehouse Details</h2>
               <label className="edit-warehouse__label">Warehouse Name</label>
               <input
-                className="edit-warehouse__input"
+                className={`edit-warehouse__input ${
+                  isWarehouseNameValid(warehouseName)
+                    ? ""
+                    : "edit-warehouse__input--invalid"
+                }`}
                 name="warehouseName"
                 type="text"
                 placeholder="Warehouse Name"
                 defaultValue={warehouseName}
-                onChange={handleChange}
+                onChange={handleWarehouseNameChange}
               ></input>
-              {!warehouseNameValid && (
+              {!isWarehouseNameValid(warehouseName) && (
                 <p className="edit-warehouse__error">
                   <img src={ErrorIcon} alt="Error icon" />
                   This field is required
@@ -134,67 +219,144 @@ alert(isWarehouseNameValid(warehouseName));
               )}
               <label className="edit-warehouse__label">Street Address</label>
               <input
-                className="edit-warehouse__input"
+                className={`edit-warehouse__input ${
+                  isAddressValid(address)
+                    ? ""
+                    : "edit-warehouse__input--invalid"
+                }`}
                 name="address"
                 type="text"
                 placeholder="Street Address"
                 defaultValue={address}
+                onChange={handleAddressChange}
               ></input>
+              {!isAddressValid(address) && (
+                <p className="edit-warehouse__error">
+                  <img src={ErrorIcon} alt="Error icon" />
+                  This field is required
+                </p>
+              )}
+
               <label className="edit-warehouse__label">City</label>
               <input
-                className="edit-warehouse__input"
+                className={`edit-warehouse__input ${
+                  isCityValid(city) ? "" : "edit-warehouse__input--invalid"
+                }`}
                 name="city"
                 type="text"
                 placeholder="City"
                 defaultValue={city}
+                onChange={handleCityChange}
               ></input>
+              {!isCityValid(city) && (
+                <p className="edit-warehouse__error">
+                  <img src={ErrorIcon} alt="Error icon" />
+                  This field is required
+                </p>
+              )}
+
               <label className="edit-warehouse__label">Country</label>
               <input
-                className="edit-warehouse__input"
+                className={`edit-warehouse__input ${
+                  isCountryValid(country)
+                    ? ""
+                    : "edit-warehouse__input--invalid"
+                }`}
                 name="country"
                 type="text"
                 placeholder="Country"
                 defaultValue={country}
+                onChange={handleCountryChange}
               ></input>
+
+              {!isCountryValid(country) && (
+                <p className="edit-warehouse__error">
+                  <img src={ErrorIcon} alt="Error icon" />
+                  This field is required
+                </p>
+              )}
             </section>
 
             <section className="edit-warehouse__contact-container">
               <h2 className="edit-warehouse__contact">Contact Details</h2>
               <label className="edit-warehouse__label">Contact Name</label>
               <input
-                className="edit-warehouse__input"
+                className={`edit-warehouse__input ${
+                  isContactNameValid(contactName)
+                    ? ""
+                    : "edit-warehouse__input--invalid"
+                }`}
                 name="contactName"
                 type="text"
                 placeholder="Contact Name"
                 defaultValue={contactName}
+                onChange={handleContactNameChange}
               ></input>
-
+              {!isContactNameValid(contactName) && (
+                <p className="edit-warehouse__error">
+                  <img src={ErrorIcon} alt="Error icon" />
+                  This field is required
+                </p>
+              )}
               <label className="edit-warehouse__label">Position</label>
               <input
-                className="edit-warehouse__input"
+                className={`edit-warehouse__input ${
+                  isContactPositionValid(contactPosition)
+                    ? ""
+                    : "edit-warehouse__input--invalid"
+                }`}
                 name="position"
                 type="text"
                 placeholder="Position"
                 defaultValue={contactPosition}
+                onChange={handlePositionChange}
               ></input>
-
+              {!isContactPositionValid(contactPosition) && (
+                <p className="edit-warehouse__error">
+                  <img src={ErrorIcon} alt="Error icon" />
+                  This field is required
+                </p>
+              )}
               <label className="edit-warehouse__label">Phone Number</label>
               <input
-                className="edit-warehouse__input"
+                className={`edit-warehouse__input ${
+                  isContactPhoneValid(contactPhone)
+                    ? ""
+                    : "edit-warehouse__input--invalid"
+                }`}
                 type="text"
                 name="phoneNumber"
                 placeholder="Phone Number"
                 defaultValue={contactPhone}
+                onChange={handleContactPhoneChange}
               ></input>
-
+              {!isContactPhoneValid(contactPhone) && (
+                <p className="edit-warehouse__error">
+                  <img src={ErrorIcon} alt="Error icon" />
+                  This field is required
+                </p>
+              )}
+          
               <label className="edit-warehouse__label">Email</label>
               <input
-                className="edit-warehouse__input"
+                className={`edit-warehouse__input ${
+                  isContactEmailValid(contactEmail)
+                    ? ""
+                    : "edit-warehouse__input--invalid"
+                }`}
                 type="text"
                 name="email"
                 placeholder="Email"
                 defaultValue={contactEmail}
+                onChange={handleContactEmailChange}
               ></input>
+
+              {!isContactEmailValid(contactEmail) && (
+                <p className="edit-warehouse__error">
+                  <img src={ErrorIcon} alt="Error icon" />
+                  This field is required
+                </p>
+              )}
             </section>
           </section>
           <section className="edit-warehouse__button">
