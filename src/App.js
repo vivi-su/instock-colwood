24e209eab913ce77d62fa7468fe1d0b0419ef3e0
Anastasia Zaika <z.anastasia11@gmail.com>
2022-11-29 01:20:27 -0800

    change app js

--- src/App.js
+++ src/App.js
@@ -23,13 +23,11 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-
   const [inventoryItemsList, setInventoryItemsList] = useState([]);
   const [warehouseList, setWarehouseList] = useState([]);
 
   //<----------------WAREHOUSE---------------------------->
   //get data for warehouse
-
   useEffect(() => {
     const fetchAllwarehouses = async () => {
       try {
@@ -45,7 +43,7 @@ function App() {
 
   function handleDeleteWarehouse(warehouseId) {
     setWarehouseList(
-      warehouseList.filter((warehouse) => warehouse.id !== warehouseId)
+      warehouseList?.filter((warehouse) => warehouse?.id !== warehouseId)
     );
   }
 
@@ -55,10 +53,10 @@ function App() {
   //   //here will go the added warehouse
   // }
 
-  function handleEditWarehouse(warehouseId) {
-    setWarehouseList.filter((warehouse) => warehouse.id === warehouseId);
-    //here will go the edited warehouse
-  }
+  // function handleEditWarehouse() {
+  //   setWarehouseList();
+  //   //here will go the edited warehouse
+  // }
 
   //<----------------INVENTORY---------------------------->
   //get data for inventory
@@ -74,7 +72,7 @@ function App() {
 
   function handleDeleteItem(itemId) {
     setInventoryItemsList(
-      inventoryItemsList.filter((item) => item.id !== itemId)
+      inventoryItemsList?.filter((item) => item?.id !== itemId)
     );
   }
 
@@ -82,10 +80,11 @@ function App() {
     setInventoryItemsList(newInventoryItem);
   }
 
-  function handleEditItem(editInventoryItem) {
-    setInventoryItemsList(editInventoryItem);
-  }
-
+  // function handleEditItem() {
+  //   setInventoryItemsList();
+  //   //here will go the edited item
+  // }
+  // console.log(inventoryItemsList);
   return (
     <BrowserRouter>
       <div className="app-container">
@@ -93,91 +92,90 @@ function App() {
         <header className="header-container">
           <Header />
         </header>
-        <main className="main-container">
-          <Routes>
-            {/*<---------------- HOME ---------------->*/}
-            {/* Home will navigate the user to the Warehouses page so there is no need for a file homePage.js */}
-            <Route path="/" element={<Navigate to={"/warehouses"} />} />
+        <div className="main-footer-wrapper">
+          {/* <div> */}
+          <main className="main-container">
+            <Routes>
+              {/*<---------------- HOME ---------------->*/}
+              {/* Home will navigate the user to the Warehouses page so there is no need for a file homePage.js */}
+              <Route path="/" element={<Navigate to={"/warehouses"} />} />
 
-            {/*<---------------- WAREHOUSE PAGE ---------------->*/}
-            {/* The Warehouses page has only one nested route to DELETE an existing warehouse*/}
-            <Route
-              path="/warehouses"
-              element={<Warehouses warehouseList={warehouseList} />}
-            >
+              {/*<---------------- WAREHOUSE PAGE ---------------->*/}
+              {/* The Warehouses page has only one nested route to DELETE an existing warehouse*/}
               <Route
-                path="deleteWarehouse/:warehouseId"
-                element={
-                  <DeleteWarehouse
-                    warehouseList={warehouseList}
-                    handleDeleteWarehouse={handleDeleteWarehouse}
-                  />
-                }
-              />
-            </Route>
-            <Route
-              path="warehouses/:warehouseId"
-              element={<WarehouseSingle />}
-            />
-            <Route
-              element={
-                <EditWarehouse
-                  warehouseList={warehouseList}
-                  handleEditWarehouse={handleEditWarehouse}
+                path="/warehouses"
+                element={<Warehouses warehouseList={warehouseList} />}
+              >
+                <Route
+                  path="deleteWarehouse/:warehouseId"
+                  element={
+                    <DeleteWarehouse
+                      warehouseList={warehouseList}
+                      handleDeleteWarehouse={handleDeleteWarehouse}
+                    />
+                  }
                 />
-              }
-              path="warehouses/editWarehouse/:warehouseId"
-            />
-            <Route path="warehouses/addWarehouse" element={<AddWarehouse />} />
-
-            {/*<---------------- INVENTORY PAGE ---------------->*/}
-            {/* The Inventory page has only one nested to DELETE an existing item*/}
-            <Route
-              path="/inventory"
-              element={<Inventory inventoryItemsList={inventoryItemsList} />}
-            >
+              </Route>
               <Route
-                path="deleteInventoryItem/:itemId"
+                path="warehouses/:warehouseId"
+                element={<WarehouseSingle />}
+              />
+              <Route
+                element={<EditWarehouse />}
+                path="warehouses/editWarehouse/:warehouseId"
+              />
+              <Route
+                path="warehouses/addWarehouse"
+                element={<AddWarehouse />}
+              />
+
+              {/*<---------------- INVENTORY PAGE ---------------->*/}
+              {/* The Inventory page has only one nested to DELETE an existing item*/}
+              <Route
+                path="/inventory"
+                element={<Inventory inventoryItemsList={inventoryItemsList} />}
+              >
+                <Route
+                  path="deleteInventoryItem/:itemId"
+                  element={
+                    <DeleteInventoryItem
+                      inventoryItemsList={inventoryItemsList}
+                      handleDeleteItem={handleDeleteItem}
+                    />
+                  }
+                />
+              </Route>
+              <Route
+                path="inventory/:itemId"
+                element={<InventoryItemDetails warehouseList={warehouseList} />}
+              />
+              <Route
+                path="inventory/editInventoryItem/:itemId"
+                element={<EditInventoryItem />}
+              />
+              <Route
+                path="inventory/addInventoryItem"
                 element={
-                  <DeleteInventoryItem
+                  <AddInventoryItem
                     inventoryItemsList={inventoryItemsList}
-                    handleDeleteItem={handleDeleteItem}
+                    handleAddItem={handleAddItem}
                   />
                 }
               />
-            </Route>
-            <Route
-              path="inventory/:itemId"
-              element={<InventoryItemDetails warehouseList={warehouseList} />}
-            />
-            <Route
-              path="inventory/editInventoryItem/:itemId"
-              element={
-                <EditInventoryItem
-                  inventoryItemsList={inventoryItemsList}
-                  handleEditItem={handleEditItem}
-                />
-              }
-            />
-            <Route
-              path="inventory/addInventoryItem"
-              element={
-                <AddInventoryItem
-                  inventoryItemsList={inventoryItemsList}
-                  handleAddItem={handleAddItem}
-                />
-              }
-            />
 
-            {/*<---------------- FALLBACK ROUTE ---------------->*/}
-            {/* Fallback route will direct user to Warehouses page*/}
-            <Route path="*" element={<Warehouses />} />
-          </Routes>
-        </main>
-        {/* Footer stays outside the routes */}
-        <footer className="footer-container">
-          <Footer />
-        </footer>
+              {/*<---------------- FALLBACK ROUTE ---------------->*/}
+              {/* Fallback route will direct user to Warehouses page*/}
+              <Route path="*" element={<Warehouses />} />
+            </Routes>
+          </main>
+          {/* </div> */}
+          {/* Footer stays outside the routes */}
+          <div className="footer-wrapper">
+            <footer className="footer-container">
+              <Footer />
+            </footer>
+          </div>
+        </div>
       </div>
     </BrowserRouter>
   );
